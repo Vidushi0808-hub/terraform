cdcafdc63cd2b05bcd889fc2b9f32a214cc3259e
Vidushi0808-hub <vidushi.bansal@knoldus.com>
2020-10-20 13:15:31 +0530

    some change

--- first_code.tf
+++ first_code.tf
@@ -1,9 +1,13 @@
+
+# It tells terraform which plugin to use and for which cloud platform are we going to create the resources for(Azure,GCP,AWS).
+
 provider "aws" {
    profile = "default"
    region  = "us-east-2"
 }
 
-resource "aws_s3_bucket" "tf_bucket" {
-   bucket = "tf_bucket_08081999"
-   acl    = "private"
+#Specify the resource. Here I have used VPC as the resource with name my_vpc
+
+resource "aws_vpc" "my_vpc" {
+   cidr_block = "10.0.0.0/16"
 }
